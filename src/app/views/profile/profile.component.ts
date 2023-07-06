04378b6c9e8bc1da4d6e021230d58df4130e6854
iftiajSoftinn <83780426+iftiaj@users.noreply.github.com>
2023-07-07 02:06:33 +0800

    Todo list app

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Todo } from "./Todo";
 
 @Component({
   selector: "app-profile",
@@ -6,6 +7,29 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ProfileComponent implements OnInit {
   constructor() {}
-
   ngOnInit(): void {}
+
+  todos : Todo[] = [];
+  newTodo : string;
+
+  saveTodo(){
+    if (this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true
+      this.todos.push(todo);
+      this.newTodo = "";
+
+
+    }
+  else{
+    alert("Please enter todo")
+  };
+}
+done(id:number){
+  this.todos[id].isCompleted = !this.todos[id].isCompleted;
+}
+remove(id:number){
+  this.todos = this.todos.filter((v,i)=> i !==id);
+}
 }
